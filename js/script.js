7dfe306c6c85ab082ac82c41c7709f31cb5c4800
partridgep <paul.partridg@gmail.com>
2020-05-05 20:12:54 -0400

    adds timeline sorting and basic DOM timeline manipulation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 /*----- constants -----*/
 const movies = [
+    {title: 'Mission Impossible 7',
+    originalRelease: '23 Jul 2021',
+    imdb: 'tt9603212'}
+    ,
     {title: 'Blue Story',
     originalRelease: '20 Mar 2020',
     imdb: 'tt9285882'}
     ,
     {title: 'A Quiet Place Part II', 
-    originalRelease: '8 Mar 2020',
+    originalRelease: '08 Mar 2020',
     imdb: 'tt8332922'}
-    
+    ,
+    {title: 'No Time To Die',
+    originalRelease: '10 Apr 2020',
+    imdb: 'tt2382320'}
+    ,
+    {title: 'Black Widow',
+    originalRelease: '11 May 2020',
+    imdb: 'tt3480822'},
+    {title: 'Monsters Inc.',
+    originalRelease: '02 Nov 2001',
+    imdb: 'tt0198781'}
+    ,
+    {title: 'Bull',
+    originalRelease: '20 Mar 2020',
+    imdb: 'tt10008784'}
 ]
+
 const apiKey = "e19d524f";
 const baseUrl = "https://www.omdbapi.com/";
 const requests = [];
-
+const originalReleaseCalendar = {};
+const releaseMonths = {};
 
 /*----- app's state (variables) -----*/
 let movieAPIData;
@@ -29,9 +49,11 @@ const $input = $('input[type="text"]')
 //initalize webpage app
 init();
 
+
 function init() {
     getAPIMovieData();
 };
+
 
 //grab movie data from API
 function getAPIMovieData() {
@@ -53,8 +75,15 @@ function getAPIMovieData() {
 
         //call function to add API data to movies array
         addAPIDataToMovies();
+
+        //place movies in original release time objects
+        organizeMoviesByOriginalRelease();
+
+        //render in DOM
+        render();
   });
 };
+
 
 function addAPIDataToMovies() {
 
@@ -65,8 +94,8 @@ function addAPIDataToMovies() {
         let movieAPI = movieAPIData[i][0];
 
         //check we have the right movie
-        console.log(movies[i].title);
-        console.log(movieAPIData[i][0].Title);
+        //console.log(movie);
+        //console.log(movieAPI);
 
         //grab relevant API data
         movie.poster = movieAPI.Poster;
@@ -76,18 +105,211 @@ function addAPIDataToMovies() {
         movie.cast = movieAPI.Actors;
         movie.plot = movieAPI.Plot;
 
-        console.log(movie);
     }
+};
+
+
+//place each movie in corresponding yearly release object
+function organizeMoviesByOriginalRelease() {
+
+    for(movie of movies) {
+
+        let releaseDate = movie.originalRelease;
+
+        //get original release year (last 4 characters of release date)
+        let originalYear = releaseDate.slice(-4);
+
+        //get original release month (middle 3 characters of release date)
+        let originalMonth = releaseDate.slice(-8, -5);
+        //get monthly index (ex. 1 for Jan)
+        originalMonth = convertMonthStrToIdx(originalMonth);
+
+        //get original release day (first 2 characters of released date)
+        let originalDay = releaseDate.slice(0, 2);
+
+        //if the release calendar does not have a year object, create it
+        if (!originalReleaseCalendar[`${originalYear}`]) {
+            originalReleaseCalendar[`${originalYear}`] = {
+                [`${originalMonth}`]: []
+                }
+            ;}
+
+        //if the release year does not have a month object, create it
+        if (!originalReleaseCalendar[`${originalYear}`][`${originalMonth}`]) {
+            originalReleaseCalendar[`${originalYear}`][`${originalMonth}`] = [];
+        }
+
+        //if the release month does not have a date object, create it
+        if (!originalReleaseCalendar[`${originalYear}`][`${originalMonth}`][`${originalDay}`]) {
+            originalReleaseCalendar[`${originalYear}`][`${originalMonth}`][`${originalDay}`] = [];
+        }
+        
+        //once those objects exist, add movie to that timeframe release bucket
+        originalReleaseCalendar[`${originalYear}`][`${originalMonth}`][`${originalDay}`].push(movie); 
+
+        //this will end up sorted because integers are sorted 
+        //numerically in objects since ES6!!!!
+
+    }
+    console.log(originalReleaseCalendar);  
+};
+
+
+//return month idx for easy calendar object sorting
+function convertMonthStrToIdx(month) {
+
+    if (month === 'Jan') {
+        month = 1;
+    }
+    else if (month === 'Feb') {
+        month = 2;
+    }
+    else if (month === 'Mar') {
+        month = 3;
+    }
+    else if (month === 'Apr') {
+        month = 4;
+    }
+    else if (month === 'May') {
+        month = 5;
+    }
+    else if (month === 'Jun') {
+        month = 6;
+    }
+    else if (month === 'Jul') {
+        month = 7;
+    }
+    else if (month === 'Aug') {
+        month = 8;
+    }
+    else if (month === 'Sep') {
+        month = 9;
+    }
+    else if (month === 'Oct') {
+        month = 10;
+    }
+    else if (month === 'Nov') {
+        month = 11;
+    }
+    else if (month === 'Dec') {
+        month = 12;
+    }
+    
+    return month;
+};
+
+//return full month string for user view in the DOM
+function convertMonthIdxToStr(month) {
+
+    if (month === '1') {
+        month = 'January';
+    }
+    else if (month === '2') {
+        month = 'February';
+    }
+    else if (month === '3') {
+        month = 'March';
+    }
+    else if (month === '4') {
+        month = 'April';
+    }
+    else if (month === '5') {
+        month = 'May';
+    }
+    else if (month === '6') {
+        month = 'June';
+    }
+    else if (month === '7') {
+        month = 'July';
+    }
+    else if (month === '8') {
+        month = 'August';
+    }
+    else if (month === '9') {
+        month = 'September';
+    }
+    else if (month === '10') {
+        month = 'October';
+    }
+    else if (month === '11') {
+        month = 'November';
+    }
+    else if (month === '12') {
+        month = 'December';
+    }
+    
+    return month;
 };
     
 
 //render in the DOM   
 function render() {
     
+    //grab entries of calendar object to make it iterable
+    let iterableCalendar = Object.entries(originalReleaseCalendar);
+    //console.log(iterableCalendar);
 
-    //sort movies array by original release date
-    //movies.sort((a, b) => b.originalRelease - a.originalRelease)
+    //iterate through each year in the calendar
+    for(yearValue of iterableCalendar) {
+        //console.log(value[1]);
+        
+        //the first value will be the year
+        let year = yearValue[0];
+        //console.log('Year is');
+        //console.log(year);
+
+        //add year to the DOM
+        $main.append(`<section class='year' id=${year}><h2>${year}</h2></section>`)
+
+        //next get iterable monthly values
+        let iterableMonths = Object.entries(yearValue[1]);
+
+        //iterate through each month in calendar
+        for(monthValue of iterableMonths) {
+
+            //the first value will be the month 
+            // (we get its long string value to render in DOM for user-friendliness)
+            let month = convertMonthIdxToStr(monthValue[0]);
+            //console.log(iterableMonths);
+            //console.log('Month is');
+            //console.log(month);
+
+            $(`#${year}`).append(`<div class='month' id=${month}${year}><h3>${month}</h3></div>`);
+
+            //next get iterable release date values
+            let iterableDates = Object.entries(monthValue[1]);
+            //console.log(iterableDates);
+
+            for(dayValue of iterableDates) {
+
+                //the first value will be the day
+                let day = dayValue[0];
+                console.log('Day is');
+                console.log(day);
+
+                $(`#${month}${year}`).append(`<div class='release' id='${day}${month}${year}'></div>`);
 
 
-    
+                //next get iterable movie releases
+                let iterableMovies = Object.entries(dayValue[1]);
+
+                for(iteratedMovie of iterableMovies) {
+
+                    //second value will be movie object
+                    let movieToAdd = iteratedMovie[1];
+
+                    addToTimeline(movieToAdd);
+                }
+                //console.log(iterableMovies);
+                //console.log(iterableMovies[0][1]);
+            }
+        }
+    } 
 };
+
+
+
+function addToTimeline(movie) {
+
+
+}
